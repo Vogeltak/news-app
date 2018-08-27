be188b9d7b78fbd4b4b61a1d412696e15dd26b56
Vogeltak <vogeltak@gmail.com>
2018-08-27 15:07:36 +0200

    Add Article data holder class and complete refactorization into separate packages

---
+++ app/src/main/java/org/maxcrone/news/data/Article.java
@@ -0,0 +1,46 @@
+package org.maxcrone.news.data;
+
+/*
+ * The Article class represents a single article
+ */
+public class Article {
+    private String title;
+    private String src;
+    private String text;
+    private String date;
+    private String url;
+    private String imgUrl;
+
+    public Article(String title, String src, String text, String date, String url, String imgUrl) {
+        this.title = title;
+        this.src = src;
+        this.text = text;
+        this.date = date;
+        this.url = url;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
