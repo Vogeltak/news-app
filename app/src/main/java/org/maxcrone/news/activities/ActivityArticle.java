959ac638522396e6228a482e2fb8030e4a477a29
Vogeltak <vogeltak@gmail.com>
2018-08-28 23:04:47 +0200

    Add menu actions for Article activity to open in browser and share article url

--- app/src/main/java/org/maxcrone/news/activities/ActivityArticle.java
+++ app/src/main/java/org/maxcrone/news/activities/ActivityArticle.java
@@ -1,10 +1,15 @@
 package org.maxcrone.news.activities;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.maxcrone.news.R;
@@ -12,6 +17,8 @@ import org.maxcrone.news.data.Article;
 import org.maxcrone.news.util.DateCalc;
 
 public class ActivityArticle extends AppCompatActivity {
+    private Article article;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,7 @@ public class ActivityArticle extends AppCompatActivity {
 
         // Retrieve Article object from the Intent Extra
         Intent intent = getIntent();
-        Article article = (Article) intent.getSerializableExtra(ActivityOverview.ARTICLE_OBJECT);
+        article = (Article) intent.getSerializableExtra(ActivityOverview.ARTICLE_OBJECT);
 
         // Retrieve all View objects for the elements of the article
         TextView sourceView = findViewById(R.id.articleSource);
@@ -45,5 +52,51 @@ public class ActivityArticle extends AppCompatActivity {
         titleView.setText(article.getTitle());
         ageView.setText(String.valueOf(DateCalc.getAgeInHours(article.getDate())) + " hours ago");
         textView.setText(article.getText());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.shareButton);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        // Create shareIntent
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, article.getUrl());
+        shareIntent.setType("text/plain");
+
+        // Set the shareIntent to the ShareActionProvider
+        //setShareIntent(shareIntent);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        // Handle item selection
+        switch (menuItem.getItemId()) {
+            case R.id.browserButton:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(article.getUrl()));
+                startActivity(browserIntent);
+                return true;
+            case R.id.shareButton:
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, article.getUrl());
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.share_article)));
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+    }
+
+    private void setShareIntent(Intent shareintent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareintent);
+        }
     }
 }
